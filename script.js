0fd871f24f59c4c148bb9e76b54c5e1fa9264c71
JinahFromBusan <jinahhwang245@gmail.com>
2022-11-13 11:19:55 +0900

    이미지 슬라이드 추가

--- script.js
+++ script.js
@@ -7,4 +7,26 @@ $(function(){
         $("#main_menu_back").stop().slideUp();
         $(".detail_menu").stop().slideUp();
     });
+
+    // 3번째 라인 왼쪽 이미지 슬라이드
+    $(".img img").eq(0).siblings().css("display","none");
+    $(".prev").addClass("off");
+    $(".prev").click(function(){
+        if(!$(".img img").first().is(":visible")){
+            $(".img img:visible").hide().prev("img").fadeIn("slow");
+            $(".next").removeClass("off");
+        }
+        if($(".img img").first().is(":visible")){
+            $(".prev").addClass("off");
+        }
+    });
+    $(".next").click(function(){
+        if(!$(".img img").last().is(":visible")){
+            $(".img img:visible").hide().next("img").fadeIn("slow");
+            $(".prev").removeClass("off");
+        }
+        if($(".img img").last().is(":visible")){
+            $(".next").addClass("off");
+        }
+    });
 });
